c670de6fe243f5632a781b854b510b9950b89f1c
Shreya Verma <72500761+shreya-v7@users.noreply.github.com>
2021-07-07 22:58:55 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ export class Home extends Component{
     render(){
         return(
             <div className="mt-5 d-flex justify-content-left">
-                This is Home
+                Home
             </div>
         )
     }
